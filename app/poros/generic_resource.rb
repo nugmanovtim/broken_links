210c8daa884f6192de25147f729e2d38285c32e2
nugmanovtim <general@nugmanovt.im>
2018-05-03 13:17:40 +0400

    Remove GenericResource

--- app/poros/generic_resource.rb
+++
@@ -1,6 +0,0 @@
-# Any web resource that isn't an HTML page
-class GenericResource < Resource
-  def find_broken_links
-    broken? ? i_am_broken : i_am_not_broken
-  end
-end
--- app/poros/resource.rb
+++ app/poros/resource.rb
@@ -1,31 +1,42 @@
 # Is a web resource of any kind
 class Resource
-  def initialize(url, body = nil)
+  attr_reader :url
+  def initialize(inspector, url, body = nil)
+    @inspector = inspector
     @url = URI(url)
     @body = body
   end
 
   def find_broken_links
-    puts @url
-    cast.find_broken_links&.flatten&.compact
+    return [] unless first_time?
+    download
+    return i_am_broken if broken?
+    if html?
+      return i_am_not_broken unless @inspector.correct_host?(@url.host)
+      HtmlPage.new(@inspector, @url, body).find_broken_links
+    else
+      i_am_not_broken
+    end
   end
 
   private
 
+  def first_time?
+    return false if @inspector.visited? @url
+    @inspector.mark_as_visited @url
+    true
+  end
+
+  def download
+    response
+  end
+
   def broken?
-    body.nil?
+    response.not_found?
   end
 
   def body
     @body ||= response.body
-  end
-
-  def cast
-    if html?
-      HtmlPage.new(@url, body)
-    else
-      GenericResource.new(@url, body)
-    end
   end
 
   def i_am_broken
