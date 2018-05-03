8132c8215b098839dc9171b22523daee8440b490
nugmanovtim <general@nugmanovt.im>
2018-05-03 13:17:57 +0400

    Introduce HttpResponse::NOT_FOUND.

--- app/poros/http_response.rb
+++ app/poros/http_response.rb
@@ -3,20 +3,22 @@ class HttpResponse
     @rest_client_response = rest_client_response
   end
 
+  ERROR = HttpResponse.new(nil)
+
+  def not_found?
+    self == ERROR
+  end
+
   # look up in headers
   def html?
+    return false if not_found?
     @rest_client_response.headers[:content_type].split(';')
                          .map { |header| header.downcase.strip }
                          .include?('text/html')
   end
 
-  def method_missing(method, *args)
-    return @rest_client_response.send(method, *args) if respond_to? method
-    super
-  end
-
-  def respond_to_missing?(method, *)
-    return true if @rest_client_response.respond_to? method
-    super
+  def body
+    return nil if not_found?
+    @rest_client_response.body
   end
 end
